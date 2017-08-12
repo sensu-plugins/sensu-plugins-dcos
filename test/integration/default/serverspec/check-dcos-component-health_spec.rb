42fd89eca585fd7fd3953f632e6b81bef0133411
Ben Abrams <me@benabrams.it>
2017-08-12 13:28:54 -0700

    wip

---
+++ test/integration/default/serverspec/check-dcos-component-health_spec.rb
@@ -0,0 +1,9 @@
+# frozen_string_literal: true
+
+require 'spec_helper'
+require 'shared_spec'
+require 'check-dcos-component-health_spec.rb'
+
+describe 'check-dcos-component-health' do
+  include_examples 'check-dcos-component-health.rb'
+end
